4e6648c6ac011cbab31e0806b246668fdb02c086
Florent-Hervers <flo230702@gmail.com>
2024-11-27 18:04:15 +0100

    Add ResGS scripts and update the paper code to make it run under pytorch

--- ResGS.py
+++ ResGS.py
@@ -1,158 +1,177 @@
+import torch
+from torch import nn
+import torch.utils.data as data
+from dataset import SNPmarkersDataset
+import wandb
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras import Model
+from scipy.stats import pearsonr
 
-from sklearn.svm import SVR
-from sklearn.linear_model import Ridge
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+class Conv1d_BN(nn.Module):
+    def __init__(self, input_size, nb_filter, kernel_size, strides=1, padding = 1):
+        super(Conv1d_BN, self).__init__()
+        self.conv = nn.Conv1d(input_size, nb_filter, kernel_size, padding= padding, stride=strides)
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm1d(nb_filter)
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.relu(x)
+        x = self.bn(x)
+        return x
 
-from DNNGP import DNNGP, DNNGP_training_set
+class Res_Block(nn.Module):
+    def __init__(self, input_size, nb_filter, kernel_size, strides=1):
+        super(Res_Block, self).__init__()
+        self.block = Conv1d_BN(input_size,nb_filter=nb_filter,kernel_size=kernel_size,strides=strides)
+    
+    def forward(self, x):
+        x = x + self.block(x)
+        return x
 
-import os
+class ResGSModel(nn.Module):
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    def __init__(self, nFilter, _KERNEL_SIZE, CHANNEL_FACTOR1, CHANNEL_FACTOR2, nlayers = 8):
+        super(ResGSModel, self).__init__()
+        self.input_block1 = Res_Block(1, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+        self.input_block2 = Res_Block(nFilter, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+        nFilter1 = int(nFilter * CHANNEL_FACTOR1)
 
-import warnings
-warnings.filterwarnings('ignore')
+        self.layers = nn.Sequential(
+            *[nn.Sequential( 
+                Conv1d_BN(int(nFilter * CHANNEL_FACTOR2**(i-1)), nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2), 
+                Conv1d_BN(nFilter1, nb_filter=int(nFilter * CHANNEL_FACTOR2**i), kernel_size=1, strides=1, padding=0), 
+                Res_Block(int(nFilter * CHANNEL_FACTOR2**i), nb_filter=int(nFilter * CHANNEL_FACTOR2**i), kernel_size=_KERNEL_SIZE, strides=1), 
+                Res_Block(int(nFilter * CHANNEL_FACTOR2**i), nb_filter=int(nFilter * CHANNEL_FACTOR2**i), kernel_size=_KERNEL_SIZE, strides=1),
+            )for i in range(1, nlayers + 1) ])
+
+        self.output = nn.Sequential(
+            Conv1d_BN(int(nFilter * CHANNEL_FACTOR2**nlayers), nb_filter= 6400 // (int(nFilter * CHANNEL_FACTOR2**nlayers)), kernel_size=1, strides=1, padding=0),
+            nn.Flatten(),
+            nn.Linear((6400 // (int(nFilter * CHANNEL_FACTOR2**nlayers))) * int(36304 / (2**nlayers)), 1)
+        )
+
+    def forward(self, x):
+        # Set the number of channels to 1 as required by the conv1d layer
+        x = x.view(x.shape[0], 1, x.shape[1])
+        
+        x = self.input_block1(x)
+        x = self.input_block2(x)
+
+        x = self.layers(x)
+
+        x = self.output(x)
+        return x
 
 
-def Conv1d_BN(x, nb_filter, kernel_size, strides=1):
-    x = layers.Convolution1D(nb_filter, kernel_size, padding='same', strides=strides, activation='relu')(x)
-    x = layers.BatchNormalization(axis=1)(x)
-    return x
-
-def Res_Block(inpt,nb_filter,kernel_size,strides=1):
-    x = Conv1d_BN(inpt,nb_filter=nb_filter,kernel_size=kernel_size,strides=strides)
-    x = layers.add([x,inpt])
-    return x
-
-def ResGSModel(inputs):
-    nFilter = 64
-    _KERNEL_SIZE = 3
+def main():
+    BATCH_SIZE = 64
+    LEARNING_RATE = 1e-3
+    DROPOUT = 0
+    N_LAYERS = 4
+    N_EPOCHS = 200
+    SCHEDULER_STEP_SIZE = 200
+    SCHEDULER_REDUCE_RATIO = 1
+    KERNEL_SIZE = 3
     CHANNEL_FACTOR1 = 4
     CHANNEL_FACTOR2 = 1.1
-    print("inputs.shape:", inputs.shape)
+    NFILTERS = 64
+    MODEL_NAME = "ResGS"
+    RUN_NAME = "first try ResGS"
 
-    x1 = Res_Block(inputs, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x1 = Res_Block(x1, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    nFilter1 = int(nFilter * CHANNEL_FACTOR1)
+    wandb.init(
+        project = "TFE",
+        config = {
+            "model_name": MODEL_NAME,
+            "batch size": BATCH_SIZE,
+            "learning_rate": LEARNING_RATE,
+            "dropout": DROPOUT,
+            "nb layers": N_LAYERS,
+            "nb epochs": N_EPOCHS,
+            "scheduler_reduce_ratio": SCHEDULER_REDUCE_RATIO,
+            "scheduler_step_size": SCHEDULER_STEP_SIZE,
+            "kernel size" : KERNEL_SIZE,
+            "channel factor 1": CHANNEL_FACTOR1,
+            "channel factor 2": CHANNEL_FACTOR2,
+            "nfilters": NFILTERS
+        },
+        name = RUN_NAME,
+        tags = ["debug"],
+    )
+    
+    train_dataset = SNPmarkersDataset(mode = "train")
+    validation_dataset = SNPmarkersDataset(mode = "validation")
+    selected_phenotypes = list(train_dataset.phenotypes.keys())
 
-    x2 = Conv1d_BN(x1 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x2 = Conv1d_BN(x2, nb_filter=nFilter, kernel_size=1, strides=1)
-    x2 = Res_Block(x2, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x2 = Res_Block(x2, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    torch.cuda.empty_cache()
 
-    x3 = Conv1d_BN(x2 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x3 = Conv1d_BN(x3, nb_filter=nFilter, kernel_size=1, strides=1)
-    x3 = Res_Block(x3, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x3 = Res_Block(x3, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+    for phenotype in selected_phenotypes:
+        
+        train_dataset.set_phenotypes = phenotype
+        validation_dataset.set_phenotypes = phenotype
 
+        train_dataloader = data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers = 4)
+        validation_dataloader = data.DataLoader(validation_dataset, batch_size=BATCH_SIZE, num_workers = 4)
 
-    x4 = Conv1d_BN(x3 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x4 = Conv1d_BN(x4, nb_filter=nFilter, kernel_size=1, strides=1)
-    x4 = Res_Block(x4, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x4 = Res_Block(x4, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+        model = ResGSModel(NFILTERS, KERNEL_SIZE, CHANNEL_FACTOR1, CHANNEL_FACTOR2, N_LAYERS)
+        print(f"Model architecture : \n {model}")
+        print(f"Numbers of parameters: {sum(p.numel() for p in model.parameters())}")
 
+        optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+        criteron = torch.nn.L1Loss()
+        model.to(device)
+        for epoch in range(N_EPOCHS):
+            train_loss = []
+            model.train()
+            for x,y in train_dataloader:
+                x,y = x.to(device), y.to(device)
+                optimizer.zero_grad()
+                output = model(x)
+                y = y.view(-1,1)
+                loss = criteron(output, y)
+                train_loss.append(loss.cpu().detach())
+                loss.backward()
+                optimizer.step()
+            
+            wandb.log({
+                    "epoch": epoch, 
+                    f"train_loss {phenotype}": np.array(train_loss).mean()
+                }
+            )
 
-    x5 = Conv1d_BN(x4 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x5 = Conv1d_BN(x5, nb_filter=nFilter, kernel_size=1, strides=1)
-    x5 = Res_Block(x5, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x5 = Res_Block(x5, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+            print(f"Finished training for epoch {epoch} for {phenotype}.")
 
-    x6 = Conv1d_BN(x5 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x6 = Conv1d_BN(x6, nb_filter=nFilter, kernel_size=1, strides=1)
-    x6 = Res_Block(x6, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x6 = Res_Block(x6, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
+            val_loss = []
+            predicted = []
+            target = []
+            model.eval()
+            for x,y in validation_dataloader:
+                x,y = x.to(device), y.to(device)
+                optimizer.zero_grad()
+                output = model(x)
+                y = y.view(-1,1)
+                loss = criteron(output, y)
+                val_loss.append(loss.cpu().detach())
+                if len(predicted) == 0:
+                    predicted = output.cpu().detach()
+                    target = y.cpu().detach()
+                else:
+                    predicted = np.concatenate((predicted, output.cpu().detach()), axis = 0)
+                    target = np.concatenate((target, y.cpu().detach()), axis = 0)
+                loss.backward()
+                optimizer.step()
+            
+            # Resize the vectors to be accepted in the pearsonr function
+            predicted = predicted.reshape((predicted.shape[0],))
+            target = target.reshape((target.shape[0],))
 
+            wandb.log({
+                    "epoch": epoch, 
+                    f"validation_loss {phenotype}": np.array(val_loss).mean(),
+                    f"correlation {phenotype}": pearsonr(predicted, target).statistic,
+                }
+            )
+            print(f"Validation step for epoch {epoch} for {phenotype} finished!")
 
-    x7 = Conv1d_BN(x6 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x7 = Conv1d_BN(x7, nb_filter=nFilter, kernel_size=1, strides=1)
-    x7 = Res_Block(x7, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x7 = Res_Block(x7, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-
-
-    x8 = Conv1d_BN(x7 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x8 = Conv1d_BN(x8, nb_filter=nFilter, kernel_size=1, strides=1)
-    x8 = Res_Block(x8, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x8 = Res_Block(x8, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-
-
-    x9 = Conv1d_BN(x8 , nb_filter=nFilter1, kernel_size=_KERNEL_SIZE, strides=2)
-    nFilter = int(nFilter * CHANNEL_FACTOR2)
-    x9 = Conv1d_BN(x9, nb_filter=nFilter, kernel_size=1, strides=1)
-    x9 = Res_Block(x9, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-    x9 = Res_Block(x9, nb_filter=nFilter, kernel_size=_KERNEL_SIZE, strides=1)
-
-    element_number = x9.shape[1] * x9.shape[2]
-    print("x9.shape[1]:", x9.shape[1])
-    print("x9.shape[2]:", x9.shape[2])
-    print("element_number:", element_number)
-    filter_near_6400 = 6400 // x9.shape[1]
-    if filter_near_6400 == 0:
-        filter_near_6400 = 1
-    print("filter_near_6400:", filter_near_6400)
-    x = Conv1d_BN(x9, nb_filter=filter_near_6400, kernel_size=1, strides=1)
-    x = layers.Flatten()(x)
-
-    x = layers.Dense(1)(x)
-
-    return Model(inputs = inputs, outputs = x)
-
-
-def ResGS_pure(X_train, X_test, y_train, y_test,
-            saveFileName,
-            CUDA_VISIBLE_DEVICES = 0,
-            Epoch = 3000,):
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(CUDA_VISIBLE_DEVICES)
-    batch_size = 64
-    patience = 1000
-    repeatTimes = 3
-    bestCorrelations = []
-    y_pres = []
-
-    ridge_model = Ridge()
-    ridge_model.fit(X_train, y_train)
-    y_pre = ridge_model.predict(X_test)
-    r_Ridge = np.corrcoef(y_pre, y_test)[0,1]  # r_Ridge is pearson correlation
-    print("r_Ridge:", r_Ridge)
-
-
-    nSNP = X_train.shape[1]
-
-    X2_train = np.expand_dims(X_train, axis=2)
-    X2_test = np.expand_dims(X_test, axis=2)
-
-    for i in range(repeatTimes):
-        tf.random.set_seed(i)
-        inputs = layers.Input(shape=(nSNP, 1))
-
-        model = ResGSModel(inputs)
-        # model.compile(loss='mse', optimizer='adam')
-        model.compile(loss='mae', optimizer='adam')
-        performance_simple = PerformancePlotCallback_ResGS_pure(X2_test, y_test, model= model, repeatTime=0,
-                                                         saveFileName=saveFileName, patience=patience)
-        history = model.fit(X2_train, y_train, epochs=Epoch, batch_size=batch_size,
-                                      validation_data=(X2_test, y_test),
-                                      verbose=0, callbacks=performance_simple)
-
-        print("performance_simple.bestCorrelation: ", performance_simple.bestCorrelation)
-        bestCorrelations.append(performance_simple.bestCorrelation)
-        y_pres.append(performance_simple.y_pre)
-
-        if performance_simple.bestCorrelation > r_Ridge:
-            break
-
-
-    print("bestCorrelation: ", max(bestCorrelations))
-    # print("==============the best result of repeatTimes is: ", bestCorrelations.index(max(bestCorrelations)))
-
-    print("bestCorrelations:", bestCorrelations)
+if __name__ == "__main__":
+    main()
