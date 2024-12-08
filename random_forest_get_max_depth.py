29f891babf92618eb41427b33eac518899654894
Florent-Hervers <flo230702@gmail.com>
2024-12-08 10:36:04 +0100

    Add scripts and results for depth analysis of random forest

---
+++ random_forest_get_max_depth.py
@@ -0,0 +1,39 @@
+from dataset import SNPmarkersDataset
+from sklearn.ensemble import RandomForestRegressor
+from scipy.stats import pearsonr
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+import time
+import pandas as pd
+
+def main():
+
+    train_dataset = SNPmarkersDataset(mode="train")
+    phenotypes = list(train_dataset.phenotypes.keys())
+
+    # These two lines are only used to compute the number of features of the training dataset for the max_features sqrt value
+    train_dataset.set_phenotypes = phenotypes[0]
+    X_train = train_dataset.get_all_SNP()
+
+    for phenotype in phenotypes:
+        start_time = time.time()
+
+        train_dataset.set_phenotypes = phenotype
+
+        X_train = train_dataset.get_all_SNP()
+        Y_train = np.array(train_dataset.phenotypes[phenotype]).ravel() / train_dataset.pheno_std[phenotype]
+
+        model = RandomForestRegressor(n_estimators=1000, random_state=2307, n_jobs=-1).fit(X_train, Y_train)
+
+        max_depth = []
+        for tree in model.estimators_:
+            max_depth.append(tree.tree_.max_depth)
+        
+        print(f"Average tree depth: {np.array(max_depth).mean()}")
+        print(f"Maximal tree depth: {max(max_depth)}")
+
+        print("////////////////////////////////////////////")
+        print(f"Computation finished in {int((time.time() - start_time) // 3600)}h {int(((time.time() - start_time) % 3600) // 60)}m {int((time.time() - start_time) % 60)}s")
+
+if __name__ == "__main__":
+    main()
