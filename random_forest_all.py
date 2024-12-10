ad105b97a18d532c43db1c1ef11fcb84053a2375
Florent-Hervers <flo230702@gmail.com>
2024-12-10 11:33:00 +0100

    Add normalization to random_forest_all

--- random_forest_all.py
+++ random_forest_all.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.metrics import mean_absolute_error
 import time
 import pandas as pd
+from utils import print_elapsed_time
 
 def main():
     selected_phenotypes = ["ep_res","de_res","FESSEp_res","FESSEa_res"]
@@ -16,11 +17,17 @@ def main():
 
     X_train = train_dataset.get_all_SNP()
     Y_train = pd.DataFrame([train_dataset.phenotypes[pheno] for pheno in selected_phenotypes]).transpose()
+    for pheno in Y_train:
+        Y_train[pheno] /= train_dataset.pheno_std[pheno]
     
+
     X_validation = validation_dataset.get_all_SNP()
     Y_validation = pd.DataFrame([validation_dataset.phenotypes[pheno] for pheno in selected_phenotypes]).transpose()
+    for pheno in Y_validation:
+        Y_validation[pheno] /= validation_dataset.pheno_std[pheno]
 
-    max_depth = [15,17,19,21,23,25,27,29,31,33,35]
+
+    max_depth = np.arange(5,19)
     max_features = [0.5, 1/3, 0.25, 0.1, 0.01, int(np.sqrt(X_train.shape[-1])), 0.001]
     nb_phenotypes = len(selected_phenotypes)
     MAE_results = np.zeros((nb_phenotypes, len(max_depth), len(max_features)))
@@ -39,10 +46,10 @@ def main():
             elif type(max_feature) == float:
                 max_nb_of_tree = int(max_feature*X_train.shape[-1])
             print(f"Max depth value tested: {max_depth_value}, max nb of features used per tree: {max_nb_of_tree}")
-            print(f"Elapsed time from start: {int((time.time() - start_time) // 60)}m {int((time.time() - start_time) % 60)}s")
+            print(f"Elapsed time from start: {print_elapsed_time(start_time)}")
             
             for k in range(nb_phenotypes):
-                MAE_results[k][i][j] = mean_absolute_error(Y_validation.iloc[:, k], predictions[:, k])
+                MAE_results[k][i][j] = mean_absolute_error(Y_validation.iloc[:, k]* validation_dataset.pheno_std[selected_phenotypes[k]], predictions[:, k] * validation_dataset.pheno_std[selected_phenotypes[k]])
                 correlation_results[k][i][j] = pearsonr(Y_validation.iloc[:, k], predictions[:, k]).statistic
                 print("--------------------------------------------")
                 print(f"Pearson correlation for {selected_phenotypes[k]}: {correlation_results[k][i][j]:.5f}")
@@ -52,14 +59,14 @@ def main():
         pd.DataFrame(MAE_results[k], 
                      index=[f"max_depth = {i}" for i in max_depth], 
                      columns=[f"max_features = {i}" for i in max_features]
-                     ).to_csv(f"Results/random_forest_all_1000_results_MAE_{selected_phenotypes[k]}.csv")
+                     ).to_csv(f"Results/random_forest_all_normalized_1000_results_MAE_{selected_phenotypes[k]}.csv")
         pd.DataFrame(correlation_results[k], 
                      index=[f"max_depth = {i}" for i in max_depth], 
                      columns=[f"max_features = {i}" for i in max_features]
-                     ).to_csv(f"Results/random_forest_all_1000_results_corr_{selected_phenotypes[k]}.csv")
+                     ).to_csv(f"Results/random_forest_all_normalized_1000_results_corr_{selected_phenotypes[k]}.csv")
     
     print("////////////////////////////////////////////")
-    print(f"Computation finished in {int((time.time() - start_time) // 3600)}h {int(((time.time() - start_time) % 3600) // 60)}m {int((time.time() - start_time) % 60)}s")
+    print(f"Computation finished in {print_elapsed_time(start_time)}")
 
 if __name__ == "__main__":
     main()
