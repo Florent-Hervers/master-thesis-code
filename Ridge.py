7a764e9b963566b685b64314208c7e2dc594fbab
Florent-Hervers <flo230702@gmail.com>
2025-01-28 15:49:03 +0100

    Correct error in ridge script + create a generic script to run a given script in different subprocess to speed computations up.

--- Ridge.py
+++ Ridge.py
@@ -1,6 +1,7 @@
 import time
 import json
 import numpy as np
+import sys
 
 from dataset import SNPmarkersDataset
 from scipy.stats import pearsonr
@@ -9,56 +10,59 @@ from sklearn.linear_model import Ridge
 from utils import print_elapsed_time
 
 def main():
+    # sys.argv[0] is the script name
+    if (len(sys.argv) < 2):
+        raise Exception(f"The phenotype to use should be given as first positional argument")
     
+    pheno = sys.argv[1]
+
     train_dataset = SNPmarkersDataset(mode="train")
     validation_dataset = SNPmarkersDataset(mode="validation")
-    phenotypes = list(train_dataset.phenotypes.keys())
     
     lambdas = np.linspace(0.001, 15, 50)
 
     MAE_results = np.zeros((len(lambdas)))
     correlation_results = np.zeros((len(lambdas)))
 
-    for pheno in phenotypes:
-        start_time = time.time()
-        iteration_counter = 0
-        
-        train_dataset.set_phenotypes = pheno
-        validation_dataset.set_phenotypes = pheno
+    start_time = time.time()
+    iteration_counter = 0
+    
+    train_dataset.set_phenotypes = pheno
+    validation_dataset.set_phenotypes = pheno
 
-        X_train = train_dataset.get_all_SNP()
-        Y_train = np.array(train_dataset.phenotypes[pheno]).ravel()
-        
-        X_validation = validation_dataset.get_all_SNP()
-        Y_validation = np.array(validation_dataset.phenotypes[pheno]).ravel()
+    X_train = train_dataset.get_all_SNP()
+    Y_train = np.array(train_dataset.phenotypes[pheno]).ravel()
+    
+    X_validation = validation_dataset.get_all_SNP()
+    Y_validation = np.array(validation_dataset.phenotypes[pheno]).ravel()
 
-        for i,lambda_val in enumerate(lambdas):
-            model = Ridge(
-                alpha= lambda_val,
-                random_state= 2307,
-            )
-            model = model.fit(X_train, Y_train)
-            validation_predictions = model.predict(X_validation)
+    for i,lambda_val in enumerate(lambdas):
+        model = Ridge(
+            alpha= lambda_val,
+            random_state= 2307,
+        )
+        model = model.fit(X_train, Y_train)
+        validation_predictions = model.predict(X_validation)
+        
+        MAE_results[i] = mean_absolute_error(Y_validation , validation_predictions) 
+        correlation_results[i] = pearsonr(Y_validation, validation_predictions).statistic
             
-            MAE_results[i] = mean_absolute_error(Y_validation , validation_predictions) 
-            correlation_results[i] = pearsonr(Y_validation, validation_predictions).statistic
-                
-            iteration_counter += 1
-            
-            print("////////////////////////////////////////////")
-            print(f"Iteration {iteration_counter}/{len(lambdas)} for phenotype {pheno} finished")
-            print(f"Lambda tested : {lambda_val}")
-            print(f"Elapsed time from start: {print_elapsed_time(start_time)}")
-            print(f"Results:")
-            print(f"    - MAE : {MAE_results[i]}")
-            print(f"    - Correlation : {correlation_results[i]}")
+        iteration_counter += 1
+        
+        print("////////////////////////////////////////////")
+        print(f"Iteration {iteration_counter}/{len(lambdas)} for phenotype {pheno} finished")
+        print(f"Lambda tested : {lambda_val}")
+        print(f"Elapsed time from start: {print_elapsed_time(start_time)}")
+        print(f"Results:")
+        print(f"    - MAE : {MAE_results[i]}")
+        print(f"    - Correlation : {correlation_results[i]}")
 
         print("////////////////////////////////////////////")
         print(f"Computation finished in {print_elapsed_time(start_time)}")
 
         with open(f"Results/ridge_{pheno}.json", "w") as f:
             results = {
-                "dim_0_values": lambdas,
+                "dim_0_values": lambdas.tolist(),
                 "dim_0_label": "lambda",
                 "correlation": correlation_results.tolist(),
                 "MAE": MAE_results.tolist()
