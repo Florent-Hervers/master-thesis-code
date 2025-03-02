386f4fb8bf13490f012c6227c9d5a69d9f10eb8e
Florent-Hervers <flo230702@gmail.com>
2025-03-02 17:37:12 +0100

    Add results for SVM and set the next run

--- SVM.py
+++ SVM.py
@@ -14,7 +14,7 @@ def main():
     phenotypes = ["FESSEa_res"] #list(train_dataset.phenotypes.keys())
 
     gammas = np.linspace(7.75e-5, 0.0001675, 5)
-    c = np.concatenate([np.array([0.9]), np.linspace(1.0, 7.75, 4)])
+    c = np.array([10.0, 12.25])
 
     MAE_results = np.zeros((len(gammas), len(c)))
     correlation_results = np.zeros((len(gammas), len(c)))
@@ -58,7 +58,7 @@ def main():
         print("////////////////////////////////////////////")
         print(f"Computation finished in {print_elapsed_time(start_time)}")
 
-        with open(f"Results/SVM_11_{pheno}.json", "w") as f:
+        with open(f"Results/SVM_12_{pheno}.json", "w") as f:
             results = {
                 "dim_0_values": gammas.tolist(),
                 "dim_0_label": "gamma",
