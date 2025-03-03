Correct masking and update the scritps to use on GPTransformer2

Models/GPTransformer.py
@@ -96,9 +96,9 @@ class GPTransformer(nn.Module):
             Returns:
                 Tensor: updated input tensor with masked values.
             """
-            idx = torch.where(torch.rand(len(x[-1])) <= probability)[0]
-            # Due to the structure of the tokenization, the 0 index isn't used
-            x[:, idx] = 0
+            idx = torch.where(torch.rand(len(x[-1])) < probability)[0]
+            # The embedding size should take into account the extra value for mask_id
+            x[:, idx] = embedding_size - 1
             return x
 
         self.mask = partial(mask_input, probability = mask_probability)