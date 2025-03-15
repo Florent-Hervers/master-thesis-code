a31a073a580459e9bc033e8b54789939c4c407d0
Florent-Hervers <flo230702@gmail.com>
2025-03-15 13:15:59 +0100

    Add implemenation of the ResNet

---
+++ Models/ResNet.py
@@ -0,0 +1,107 @@
+import math
+import torch
+import torch.nn.functional as F
+
+from torch import nn
+from typing import List
+from omegaconf import ListConfig
+
+class ResNetBlock(nn.Module):
+    def __init__(self, kernel_size:int , input_channel_size: int, nlayers: int, reduce_first_layer: bool = False):
+        super(ResNetBlock, self).__init__()
+        
+        if torch.cuda.is_available():
+            self.device = "cuda:0"
+        else:
+            self.device = "cpu"
+
+        if reduce_first_layer:
+            output_channel_size = input_channel_size * 2
+            first_layer_stride = 2
+            self.residual_conv = nn.Conv1d(input_channel_size, output_channel_size, 1, stride=2)
+        else:
+            output_channel_size = input_channel_size
+            first_layer_stride = 1
+            self.residual_conv = None
+        
+        layers = [
+            nn.Conv1d(input_channel_size, output_channel_size, kernel_size, padding=1, stride=first_layer_stride),
+            nn.BatchNorm1d(output_channel_size)
+        ]
+        
+        for _ in range(1, nlayers + 1):
+            layers.append(nn.ReLU())
+            layers.append(nn.Conv1d(output_channel_size, output_channel_size, kernel_size, padding=1))
+            layers.append(nn.BatchNorm1d(output_channel_size))
+
+        self.block = nn.Sequential(*layers).to(self.device)
+
+    def forward(self, x):
+        y = self.block(x)
+
+        if self.residual_conv:
+            x = self.residual_conv(x)
+        
+        return F.relu(y + x)
+    
+
+class ResNet(nn.Module):
+    def __init__(self, start_channel_size: int, kernel_size: int, layers_size: List[int], regressor_hidden_size: List[int]):
+        super(ResNet,self).__init__()
+
+        # In the case where the parameters are given via hydra configs file, the type of arguments are ListConfig and not list.
+        if type(regressor_hidden_size) == ListConfig:
+            regressor_hidden_size = list(regressor_hidden_size)
+
+        if type(layers_size) == ListConfig:
+            layers_size = list(layers_size)
+
+        if torch.cuda.is_available():
+            self.device = "cuda:0"
+        else:
+            self.device = "cpu"
+
+        self.conv = nn.Conv1d(1, start_channel_size, 7, stride=2, padding=3).to(self.device)
+        self.max_pool = nn.MaxPool1d(3, stride=2, padding=1).to(self.device)
+
+        layers = [
+            ResNetBlock(kernel_size, start_channel_size, layers_size[0])
+        ]
+
+        for i, size in enumerate(layers_size[1:]):
+            layers.append(
+                ResNetBlock(kernel_size, start_channel_size * (2**i), size, reduce_first_layer=True)
+            )
+        
+        self.layers = nn.Sequential(*layers).to(self.device)
+
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.flatten = nn.Flatten()
+        
+        if len(regressor_hidden_size) == 0:
+            raise Exception("An empty list isn't a valid input for the regressor_hidden_size parameter.")
+        
+        regressor_input_size = start_channel_size * (2**(len(layers_size) - 1))
+        mlp_layers = [nn.Linear(regressor_input_size, regressor_hidden_size[0])]
+        for i in range(1, len(regressor_hidden_size)):
+            mlp_layers.append(nn.ReLU())
+            mlp_layers.append(nn.Linear(regressor_hidden_size[i-1], regressor_hidden_size[i]))
+        mlp_layers.append(nn.ReLU())
+        mlp_layers.append(nn.Linear(regressor_hidden_size[-1], 1))
+
+        self.output = nn.Sequential(*mlp_layers).to(self.device)
+
+    def forward(self, x):
+
+        if len(x.shape) == 2:
+            x = x.view(x.shape[0], 1, x.shape[1])
+        
+        if len(x.shape) != 3:
+            raise Exception(f"Bad dimention for the input, only 2 or 3 dimentional input are allowed but got {len(x.shape)} instead!")
+        
+        x = self.conv(x)
+        x = self.max_pool(x)
+        x = self.layers(x)
+        x = self.avg_pool(x)
+        x = self.flatten(x)
+        return self.output(x)
