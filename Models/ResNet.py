e432995c73968d125835976503ca5e561317b693
Florent-Hervers <flo230702@gmail.com>
2025-03-20 13:35:53 +0100

    Update the ResNet to enable training without pooling layers

--- Models/ResNet.py
+++ Models/ResNet.py
@@ -46,7 +46,10 @@ class ResNetBlock(nn.Module):
     
 
 class ResNet(nn.Module):
-    def __init__(self, start_channel_size: int, kernel_size: int, layers_size: List[int], regressor_hidden_size: List[int]):
+    valid_aggregation = ["pooling", "strided"]
+    
+
+    def __init__(self, start_channel_size: int, kernel_size: int, layers_size: List[int], regressor_hidden_size: List[int], aggregation: str = "pooling"):
         super(ResNet,self).__init__()
 
         # In the case where the parameters are given via hydra configs file, the type of arguments are ListConfig and not list.
@@ -61,8 +64,15 @@ class ResNet(nn.Module):
         else:
             self.device = "cpu"
 
+        if aggregation not in self.valid_aggregation:
+            raise Exception(f"Valid aggregation values are {self.valid_aggregation} but got {aggregation} instead!")
+        
         self.conv = nn.Conv1d(1, start_channel_size, 7, stride=2, padding=3).to(self.device)
-        self.max_pool = nn.MaxPool1d(3, stride=2, padding=1).to(self.device)
+        
+        if aggregation == self.valid_aggregation[0]:
+            self.aggreg1 = nn.MaxPool1d(3, stride=2, padding=1).to(self.device)
+        elif aggregation == self.valid_aggregation[1]:
+            self.aggreg1 = nn.Conv1d(start_channel_size, start_channel_size, 3, stride=2, padding=1).to(self.device)
 
         layers = [
             ResNetBlock(kernel_size, start_channel_size, layers_size[0])
@@ -75,13 +85,18 @@ class ResNet(nn.Module):
         
         self.layers = nn.Sequential(*layers).to(self.device)
 
-        self.avg_pool = nn.AdaptiveAvgPool1d(1)
-        self.flatten = nn.Flatten()
+        if aggregation == self.valid_aggregation[0]:
+            self.aggreg2 = nn.AdaptiveAvgPool1d(1).to(self.device)
+            regressor_input_size = start_channel_size * (2**(len(layers_size) - 1))
+        elif aggregation == self.valid_aggregation[1]:
+            self.aggreg2 = nn.Conv1d(start_channel_size * (2**(len(layers_size)-1)), 1, 1).to(self.device)
+            regressor_input_size = math.ceil(36304 / (2**(len(layers_size) + 1)))
+
+        self.flatten = nn.Flatten().to(self.device)
         
         if len(regressor_hidden_size) == 0:
             raise Exception("An empty list isn't a valid input for the regressor_hidden_size parameter.")
         
-        regressor_input_size = start_channel_size * (2**(len(layers_size) - 1))
         mlp_layers = [nn.Linear(regressor_input_size, regressor_hidden_size[0])]
         for i in range(1, len(regressor_hidden_size)):
             mlp_layers.append(nn.ReLU())
@@ -100,8 +115,8 @@ class ResNet(nn.Module):
             raise Exception(f"Bad dimention for the input, only 2 or 3 dimentional input are allowed but got {len(x.shape)} instead!")
         
         x = self.conv(x)
-        x = self.max_pool(x)
+        x = self.aggreg1(x)
         x = self.layers(x)
-        x = self.avg_pool(x)
+        x = self.aggreg2(x)
         x = self.flatten(x)
         return self.output(x)
