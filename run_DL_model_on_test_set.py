fad28feadd548d8698613c3f219a620696cb807c
Florent-Hervers <flo230702@gmail.com>
2025-05-06 09:40:11 +0200

    Add modifcation to evaluate Shallow_MLP_all on the test set

--- run_DL_model_on_test_set.py
+++ run_DL_model_on_test_set.py
@@ -1,4 +1,5 @@
 
+from sklearn.linear_model import Ridge
 import torch
 import numpy as np
 import pandas as pd
@@ -8,12 +9,13 @@ from hydra import compose, initialize
 from hydra.utils import instantiate
 from argparse import ArgumentParser
 from Models.GPTransformer import EmbeddingType
-from utils import convert_categorical_to_frequency
+from utils import convert_categorical_to_frequency, format_batch
 from os.path import isfile, join
 from scipy.stats import pearsonr
 from dataset import SNPmarkersDataset, SNPResidualDataset
 from torch.utils.data import DataLoader
 from sklearn.feature_selection import mutual_info_regression
+from functools import partial
 
 def evaluate_models(checkpoint_directory: str):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -23,7 +25,14 @@ def evaluate_models(checkpoint_directory: str):
     for model_file in [f for f in listdir(checkpoint_directory) if isfile(join(checkpoint_directory, f))]:
         model_filename = model_file.split(".")[0]
         model_name = model_filename.split("_")[0]
-        phenotype = model_filename.split("_")[-2] + "_" + model_filename.split("_")[-1] 
+        if model_filename.split("_")[-1] == "res":
+            phenotype = model_filename.split("_")[-2] + "_" + model_filename.split("_")[-1]
+        elif model_filename.split("_")[-1] == "all":
+            phenotype = ["ep_res", "de_res", "FESSEp_res", "FESSEa_res"]
+        else:
+            print(f"WARNING: Bad structured filename for {model_file}")
+            continue
+            
         
         # Only to test that the script works as intended
         if (model_filename != "GPTransformer_size_res" and model_name == "GPTransformer") or phenotype != "ep_res":
@@ -109,6 +118,46 @@ def evaluate_models(checkpoint_directory: str):
                 sequence_length = len(X_test.iloc[0])
                 embedding_type = EmbeddingType.EmbeddingTable
 
+            elif model_name == "ReGS":
+                original_train_dataset = SNPmarkersDataset(mode = "train")
+                original_test_dataset = SNPmarkersDataset(mode = "test")
+                original_validation_dataset = SNPmarkersDataset(mode = "validation")
+
+                # Best ridge hyperparameters for all the phenotypes
+                hp = {
+                    "ep_res": {"lambda": 55600},
+                    "de_res": {"lambda": 44500},
+                    "FESSEp_res": {"lambda": 26250},
+                    "FESSEa_res": {"lambda": 34000},
+                    "size_res": {"lambda": 20900},
+                    "MUSC_res": {"lambda": 23950},
+                }
+                
+                original_test_dataset.set_phenotypes = phenotype
+                original_validation_dataset.set_phenotypes = phenotype
+
+                X_train = original_train_dataset.get_all_SNP()
+                y_train = original_train_dataset.phenotypes[phenotype]
+
+                X_val = original_validation_dataset.get_all_SNP()
+                y_val = original_validation_dataset.phenotypes[phenotype]
+                
+                ridge_model = Ridge(alpha= hp[phenotype]["lambda"])
+                    
+                ridge_model.fit(X_train, y_train)
+               
+                y_test_pre = ridge_model.predict(X_test)
+                y_val_pre = ridge_model.predict(X_val)
+
+                y_test = y_test - y_test_pre
+                y_val = y_val - y_val_pre
+
+                y_test = y_test.to_numpy(dtype=np.float32)
+                y_val = y_val.to_numpy(dtype=np.float32)
+                
+                test_dataset = SNPResidualDataset(X_test.to_numpy(dtype=np.float32), y_test)
+                validation_dataset = SNPResidualDataset(X_val.to_numpy(dtype=np.float32), y_val)
+
             else:
                 validation_dataset = SNPmarkersDataset(mode = "validation")
                 test_dataset = SNPmarkersDataset(mode = "test")
@@ -116,8 +165,13 @@ def evaluate_models(checkpoint_directory: str):
                 validation_dataset.set_phenotypes = phenotype
                 test_dataset.set_phenotypes = phenotype
 
-            validation_dataloader = DataLoader(validation_dataset, batch_size=32, shuffle=False)
-            test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+            if model_filename.split("_")[-1] == "all":
+                validation_dataloader = DataLoader(validation_dataset, batch_size=32, shuffle=False, collate_fn=partial(format_batch, phenotypes=phenotype))
+                test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, collate_fn=partial(format_batch, phenotypes=phenotype))
+            else:
+                validation_dataloader = DataLoader(validation_dataset, batch_size=32, shuffle=False)
+                test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+            
             
         except Exception as e:
             print(f"The following error occured when preprocessing data for the model for {model_filename}: \n{e}")
@@ -144,7 +198,10 @@ def evaluate_models(checkpoint_directory: str):
                     embedding_type = embedding_type
                 )
             else:
-                model = instantiate(model_cfg.model_config.model)
+                model = instantiate(
+                    model_cfg.model_config.model,
+                    output_size = len(phenotype) if type(phenotype) == list else 1
+                )
 
             model.load_state_dict(torch.load(join(checkpoint_directory, model_file,), weights_only=False))
             model.eval()
@@ -179,6 +236,14 @@ def evaluate_models(checkpoint_directory: str):
                     predicted = predicted.reshape((predicted.shape[0],) if type(phenotype) == str else (predicted.shape[0],len(phenotype))) 
                     target = target.reshape((target.shape[0],) if type(phenotype) == str else (target.shape[0],len(phenotype)))
                     
+                    if model_name == "ResGS":
+                        if set_name == "validation":
+                            predicted += y_val_pre
+                            target += y_val_pre
+                        elif set_name == "test":
+                            predicted += y_test_pre
+                            target += y_test_pre
+
                     print(f"{model_name} results on the {set_name} set for the phenotype {phenotype}:")
                     print(f"    - MAE: {np.array(test_MAE).mean()}")
                     print(f"    - Correlation: {pearsonr(predicted, target).statistic}")
